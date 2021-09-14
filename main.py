8e91d69f7204d8bbce1d8abb8252b45b59fe3ec8
trinhtuanvu <vutuantrinh2000@gmail.com>
2021-09-14 10:24:20 +0700

    change output

--- main.py
+++ main.py
@@ -15,6 +15,7 @@ if __name__ == '__main__':
 
     if args.library == "wavaugment" : 
         for file in os.listdir(args.input):
+            folder = file.split("_")[1]
             path = os.path.join(args.input,file)
             data, sampling_rate = torchaudio.load(path)
             augment = WavAugment_lib.aug_factory(args.chain, sample_rate=SAMPLE_RATE,args=args)
@@ -22,13 +23,16 @@ if __name__ == '__main__':
             y = augment.apply(data, 
                     src_info=dict(rate=sampling_rate, length=data.size(1), channels=data.size(0)),
                     target_info=dict(rate=sampling_rate, length=0))
-            torchaudio.save("{}/wavaugment_{}.wav".format(args.output,file[:-4]), y, SAMPLE_RATE)
+            effects = args.chain.replace(",",".")
+            torchaudio.save("{}/{}/{}_{}.wav".format(args.output,folder,file[:-4],effects), y, SAMPLE_RATE)
 
     if args.library == "audiomentations" : 
         for file in os.listdir(args.input):
+            folder = file.split("_")[1]
             path = os.path.join(args.input,file)
             data,sr = librosa.load(path,sr= SAMPLE_RATE)
             augment = audiomentations_lib.aug_factory(args.compose, args)
             y = augment(samples=data, sample_rate=SAMPLE_RATE)
             y = convert_float_samples_to_int16(y)
-            soundfile.write("{}/audiomentations_{}.wav".format(args.output,file[:-4]), y, samplerate=SAMPLE_RATE)
+            effects = args.compose.replace(",",".")
+            soundfile.write("{}/{}/{}_{}.wav".format(args.output,folder,file[:-4],effects), y, samplerate=SAMPLE_RATE)
