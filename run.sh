4c48364ccfefb3f453526dde5c43eeffe07656ef
Tuan Vu Trinh <vutuantrinh2000@gmail.com>
2021-12-30 09:58:55 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,12 +2,12 @@
 
 for folder in ~/get-data-TTS/data/*/;
 do 
-    # python3 main.py -input "$file" \
-    #                 -output "/home/trinhtuanvu/get-data-TTS/augment-data/" \
+    # python3 main.py -input "$folder" \
+    #                 -output "your output folder" \
     #                 wavaugment \
     #                 --chain "time_drop"
     python3 main.py -input "$folder" \
-                    -output "/home/trinhtuanvu/get-data-TTS/augment-data/" \
+                    -output "your output folder" \
                     audiomentations \
                     --compose "TimeMask"
 # (PitchShift,AddGaussianSNR),(Shift,Gain),(Clip,FrequencyMask),(TimeMask)
